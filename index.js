56a112fb6f7a7c332e13c22b250ebb178fbd9bc6
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-26 16:57:07 -0400

    post

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//POST
+server.post('/api/zoos', (req, res) => {
+  const name = req.body;
+  db.insert(name).into('zoos')
+  .then(ids => {
+    const id = ids[0];
+    db('zoos')
+      .where({ id })
+      .first()
+      .then(data => {
+        res.status(201).json(data);
+      });
+    }).catch(err=>res.status(500).json(err))
+})
 
 const port = 3300;
 server.listen(port, function() {
