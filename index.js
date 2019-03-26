2f8597a6fd067c31311ba3f3cef365b8aa586f04
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-26 17:29:27 -0400

    post fixws

--- index.js
+++ index.js
@@ -11,18 +11,28 @@ server.use(helmet());
 // endpoints here
 //POST
 server.post('/api/zoos', (req, res) => {
-  const name = req.body;
-  db.insert(name).into('zoos')
-  .then(ids => {
-    const id = ids[0];
-    db('zoos')
-      .where({ id })
-      .first()
-      .then(data => {
-        res.status(201).json(data);
+  const {name}= req.body;
+  db('zoos').insert({name})
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({ 
+        error: "Some useful error message. Your post request is no good" 
       });
-    }).catch(err=>res.status(500).json(err))
+    });
+});
+
+//GET ZOOs
+server.get('/', (req,res)=>{
+  db.select().from('zoos')
+  .then(data =>{ res.status(200).json(data)})
+  .catch(()=>res.status(500).json({error: "Some useful error ocurred while trying to get data"}))
 })
+
+//GET ZOOs BY ID
+
+
 
 const port = 3300;
 server.listen(port, function() {
