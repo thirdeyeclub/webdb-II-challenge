bac338dba883e30ca1e2b5cb3cf17d7ffd6ae679
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-03-26 18:06:42 -0400

    put broken

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 const server = express();
-const knex = require('knex');
-const knexConfig = require('./knexfile');
+const knex = require("knex");
+const knexConfig = require("./knexfile");
 const db = knex(knexConfig.development);
 
 server.use(express.json());
@@ -10,29 +10,69 @@ server.use(helmet());
 
 // endpoints here
 //POST
-server.post('/api/zoos', (req, res) => {
-  const {name}= req.body;
-  db('zoos').insert({name})
+server.post("/api/zoos", (req, res) => {
+  const { name } = req.body;
+  db("zoos")
+    .insert({ name })
     .then(data => {
       res.status(201).json(data);
     })
     .catch(err => {
-      res.status(500).json({ 
-        error: "Some useful error message. Your post request is no good" 
+      res.status(500).json({
+        error: "Some useful error message. Your post request is no good"
       });
     });
 });
 
 //GET ZOOs
-server.get('/', (req,res)=>{
-  db.select().from('zoos')
-  .then(data =>{ res.status(200).json(data)})
-  .catch(()=>res.status(500).json({error: "Some useful error ocurred while trying to get data"}))
-})
+server.get("/api/zoos", (req, res) => {
+  db.select()
+    .from("zoos")
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "Some useful error ocurred while trying to get data" })
+    );
+});
 
 //GET ZOOs BY ID
+server.get("/api/zoos/:id", (req, res) => {
+  const id = req.params.id;
+  db("zoos")
+    .where({ id: id })
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "Some useful error ocurred while trying to get data" });
+    });
+});
 
-
+//PUT
+server.put("/api/zoos/:id", (req, res) => {
+  const name = req.body.name;
+  const id = req.params.id;
+  db("zoos")
+    .where({ id })
+    .update(name)
+    .then(count => {
+      if (count > 0) {
+        name => {
+          res.status(200).json(name);
+        };
+      } else {
+        res.status(404).json({ message: "Record not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
